a04ff62655543021891ae2b32eef3cc75491b643
Rogério Araújo <rogerio.araujo@mail.com>
2019-02-28 00:00:20 -0300

    Adicionando comentários sobre a persistência de dados

--- jpa-example-crud/src/main/java/nf/co/rogeiroaraujo/jpa_crud/JPAUtil/PersistenceData.java
+++ jpa-example-crud/src/main/java/nf/co/rogeiroaraujo/jpa_crud/JPAUtil/PersistenceData.java
@@ -5,8 +5,10 @@ import nf.co.rogeiroaraujo.jpa_crud.Model.Animal;
 
 import javax.persistence.EntityManager;
 
+// classe que persiste os dados no banco
 public class PersistenceData implements Persistence {
 
+    // criando a instancia do gerenciador de entidades
     private final EntityManager em;
 
     public PersistenceData(EntityManager em) {
@@ -14,14 +16,23 @@ public class PersistenceData implements Persistence {
     }
     
     public void insert(){
+        
+        // criação dos objetos o qual serão persistidos
         Animal animal = new Animal("Dog", "Rex");
         Animal animal_2 = new Animal("Bird", "Piu Piu");
         Animal animal_3 = new Animal("Cat", "Tom");
 
+        // abrindo uma nova transaçao com o banco de dados
         em.getTransaction().begin();
+        // persistindo os objetos criados anteriormente
         em.persist(animal);
         em.persist(animal_2);
         em.persist(animal_3);
+        
+        // criando a sincronização dos dados
+        em.flush();
+        
+        // confirmação da persistencia no banco de dados
         em.getTransaction().commit();
     }
 }
