b38a6e476450713e4d24908ed8207afa11db7eb6
Rogério Araújo <rogerio.araujo@mail.com>
2019-02-28 00:06:25 -0300

    Adicionando comentários na classe main

--- jpa-example-crud/src/main/java/nf/co/rogeiroaraujo/jpa_crud/Loader.java
+++ jpa-example-crud/src/main/java/nf/co/rogeiroaraujo/jpa_crud/Loader.java
@@ -15,20 +15,28 @@ public class Loader {
 //        final String PERSISTENCE_UNIT_NAME = "PERSISTENCE_HIBERNATE";
         final String PERSISTENCE_UNIT_NAME = "PERSISTENCE_ECLIPSE";
 
+        //instacia o EntityManager
         EntityManager em = Persistence
                 .createEntityManagerFactory(PERSISTENCE_UNIT_NAME)
                 .createEntityManager();
 
+        // instancia a classe de persistencia
         new PersistenceData(em).insert();
 
+        // exibe (select) todas as informações da tabela do banco de dados
         allAnimals(em);
     }
     
+    // metodo que busca todas as informações no banco de dados
     private static void allAnimals(EntityManager em) {
+        // define a consulta JPQL
         String sql = "SELECT a FROM Animal a";
+        // define o tipo de consulta, e o tipo
         TypedQuery<Animal> query = em.createQuery(sql, Animal.class);
+        // define o tipo de retorno da consulta
         List<Animal> resultList = query.getResultList();
-
+        
+        // imprime o nome de todos os animais selecionados
         forEach(resultList);
     }
 
